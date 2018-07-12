fc2e0f2323800b986d36dec334023e8012426cab
debugmodedotnet <debugmode@outlook.com>
2018-07-12 16:25:03 +0530

    version 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// import { IgxGridModule } from "igniteui-angular";
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent, data: { text: 'Home' }}
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent, data: { text: "Home" } }
 ];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes), BrowserAnimationsModule],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,19 +32,13 @@ export class HomeComponent implements OnInit {
   public addNewSlide() {
     this.slides.push(
       {
-        description: "30+ Material-based Angular components to code speedy web apps faster.",
-        heading: "Ignite UI for Angular",
         image: "./assets/avengers.jpg"
       },
       {
-        description: "A complete JavaScript UI library empowering you to build data-rich responsive web apps.",
-        heading: "Ignite UI for Javascript",
-        image: "https://www.infragistics.com/angular-demos/assets/images/carousel/slide2-ignite.png"
+        image: "./assets/avengers.jpg"
       },
       {
-        description: "Build full-featured business apps with the most versatile set of ASP.NET AJAX UI controls",
-        heading: "Ultimate UI for ASP.NET",
-        image: "https://www.infragistics.com/angular-demos/assets/images/carousel/slide3-aspnet.png"
+        image: "./assets/avengers.jpg"
       }
     );
   }
@@ -64,31 +58,36 @@ export class HomeComponent implements OnInit {
       isFavorite: false,
       name: "PFS Roxy Theatere",
       phone: "2023 Sansom Ft",
-      photo: "https://www.infragistics.com/angular-demos/assets/images/men/27.jpg"
+      photo: "https://www.infragistics.com/angular-demos/assets/images/men/27.jpg",
+      color: 'red'
     },
     {
       isFavorite: true,
       name: "Landmark's Ritz Bourse",
       phone: "400 Ranstead St",
-      photo: "https://www.infragistics.com/angular-demos/assets/images/men/1.jpg"
+      photo: "https://www.infragistics.com/angular-demos/assets/images/men/1.jpg",
+      color: 'blue'
     },
     {
       isFavorite: false,
       name: "Landmark's Ritz East",
       phone: "125 S 2nd St",
-      photo: "https://www.infragistics.com/angular-demos/assets/images/women/50.jpg"
+      photo: "https://www.infragistics.com/angular-demos/assets/images/women/50.jpg",
+      color: 'green'
     },
     {
       isFavorite: false,
       name: "Lisa Landers",
       phone: "901-747-3428",
-      photo: "https://www.infragistics.com/angular-demos/assets/images/women/3.jpg"
+      photo: "https://www.infragistics.com/angular-demos/assets/images/women/3.jpg",
+      color: 'red'
     },
     {
       isFavorite: true,
       name: "Dorothy H. Spencer",
       phone: "573-394-9254",
-      photo: "https://www.infragistics.com/angular-demos/assets/images/women/67.jpg"
+      photo: "https://www.infragistics.com/angular-demos/assets/images/women/67.jpg",
+      color: 'blue'
     }
   ];
 }
